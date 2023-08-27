e40b1be39e10de3dfb48d7d5496cf97166595807
jayja <jayja@JJTop.unl.edu>
2023-08-27 17:46:06 -0500

    added my name and major

--- Hello.java
+++ Hello.java
@@ -3,6 +3,8 @@ public class Hello {
   public static void main(String args[]) {
     System.out.println("John!");
     System.out.println("My Major is Marketing!");
+    System.out.println("JayJay!");
+    System.out.println("My Major is Computer Science!");
   }
 
 }
